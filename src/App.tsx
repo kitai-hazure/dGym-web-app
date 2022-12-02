1e9d717b26d6a986c05bbb54ced7e60c9f907f1a
kalashshah <202051096@iiitvadodara.ac.in>
2022-12-03 03:22:13 +0530

    :tada: animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Authentication from "./pages/Authentication/index";
 import './App.css';
