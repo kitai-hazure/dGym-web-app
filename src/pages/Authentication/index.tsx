7b2307d0f29a1d0784297126713c0bab3da06a7b
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-12-03 01:50:23 +0530

    integrated metamask

---
+++ src/pages/Authentication/index.tsx
@@ -0,0 +1,45 @@
+import { useContext } from "react";
+import { MetaMaskContext } from "../../hooks/useMetamask";
+
+const Authentication = () => {
+    // @ts-ignore
+  const {haveMetamask,isConnected,accountAddress,accountBalance,connectWallet} = useContext(MetaMaskContext);
+  return (
+    <div className="App">
+      <header className="App-header">
+        {haveMetamask ? (
+          <div className="App-header">
+            {isConnected ? (
+              <div className="card">
+                <div className="card-row">
+                  <h3>Wallet Address:</h3>
+                  <p>
+                    {accountAddress.slice(0, 4)}...
+                    {accountAddress.slice(38, 42)}
+                  </p>
+                </div>
+                <div className="card-row">
+                  <h3>Wallet Balance:</h3>
+                  <p>{accountBalance}</p>
+                </div>
+              </div>
+            ) : (
+              <p>Please connect to metamask :)</p>
+            )}
+            {isConnected ? (
+              <p className="info">🎉 Connected Successfully</p>
+            ) : (
+              <button className="btn" onClick={connectWallet}>
+                Connect
+              </button>
+            )}
+          </div>
+        ) : (
+          <p>Please Install MataMask</p>
+        )}
+      </header>
+    </div>
+  );
+};
+
+export default Authentication;
