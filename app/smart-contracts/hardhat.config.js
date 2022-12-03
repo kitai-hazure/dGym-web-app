8742cd0a312d4967d00772cff618cfdf99985b3c
AkhileshManda <manda.akhilesh4@gmail.com>
2022-12-03 07:54:02 +0530

    Added smart contract deployments

--- app/smart-contracts/hardhat.config.js
+++ app/smart-contracts/hardhat.config.js
@@ -1,45 +1,31 @@
-require("@nomicfoundation/hardhat-toolbox");
-require('@nomiclabs/hardhat-waffle');
+// require("@nomicfoundation/hardhat-toolbox");
+// require('@nomiclabs/hardhat-waffle');
 
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.17",
+// };
+
+require("@nomicfoundation/hardhat-toolbox");
+require('hardhat-deploy');
+require('hardhat-deploy-ethers');
+require("dotenv").config()
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "wallaby",
+  networks: {
+    wallaby: {
+      url: "https://wallaby.node.glif.io/rpc/v0",
+      accounts: [PRIVATE_KEY],
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
 };
-
-
-// ------------ QUICK NODE CONFIGS ------------
-
-// require("@nomiclabs/hardhat-ethers");
-// require("@nomicfoundation/hardhat-toolbox");
-// require('@nomiclabs/hardhat-waffle');
-// require('dotenv').config();
-
-// module.exports = {
-//   defaultNetwork: "matic",
-//   networks: {
-//     hardhat: {
-//     },
-//     matic: {
-//       url: "https://maximum-wild-gadget.matic-testnet.discover.quiknode.pro/863e129e6ff07f3dbb9d5c6bbc5d003c537de67a/",
-//       accounts: [process.env.PRIVATE_KEY]
-//     }
-//   },
-//   solidity: {
-//     version: "0.8.17",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200
-//       }
-//     }
-//   },
-//   paths: {
-//     sources: "./contracts",
-//     tests: "./test",
-//     cache: "./cache",
-//     artifacts: "./artifacts"
-//   },
-//   mocha: {
-//     timeout: 20000
-//   }
-// }
--- app/smart-contracts/test/ExerciseStaking.js
+++ app/smart-contracts/test/ExerciseStaking.js
@@ -1,12 +1,19 @@
 const {expect} = require ('chai');
+const {ethers} = require ('hardhat');
 
 describe ('Exercise Staking', function () {
   describe ('This tests our Exercise Staking Contract', function () {
-    it ('should deploy our contract', async function () {
+    it ('should deploy our contract', async function (taskArgs) {
+      const contractAddr = taskArgs.contract;
+      const account = taskArgs.account;
       const [acc1, acc2] = await ethers.getSigners ();
-      const ExerciseStaking = await ethers.getContractFactory ('Staking', acc1);
-      const exerciseStaking = await ExerciseStaking.deploy ();
-      await exerciseStaking.deployed ();
+      const ExerciseStaking = await ethers.getContractFactory ('Staking');
+      const exerciseStaking = new ethers.Contract (
+        contractAddr,
+        ExerciseStaking.interface,
+        acc1
+      );
+      // await exerciseStaking.deployed ();
       expect (await exerciseStaking.getOwner ()).to.equal (acc1.address);
     });
   });
@@ -17,14 +24,15 @@ describe ('Exercise Staking', function () {
         const provider = waffle.provider;
         let contractBalance;
         let acc1Balance;
+        const contractAddr = taskArgs.contract;
+        const account = taskArgs.account;
         const [acc1, acc2] = await ethers.getSigners ();
-
-        const ExerciseStaking = await ethers.getContractFactory (
-          'Staking',
+        const ExerciseStaking = await ethers.getContractFactory ('Staking');
+        const exerciseStaking = new ethers.Contract (
+          contractAddr,
+          ExerciseStaking.interface,
           acc1
         );
-        const exerciseStaking = await ExerciseStaking.deploy ();
-        await exerciseStaking.deployed ();
 
         const amount = ethers.utils.parseEther ('0.01');
         contractBalance = await provider.getBalance (exerciseStaking.address);
@@ -53,15 +61,17 @@ describe ('Exercise Staking', function () {
     });
   });
   describe ('This should add the real target achieved by user', function () {
-    it ('this would set the target done value if the user does it under one day', async () => {
+    it ('this would set the target done value if the user does it under one day', async taskArgs => {
       try {
+        const contractAddr = taskArgs.contract;
+        const account = taskArgs.account;
         const [acc1, acc2] = await ethers.getSigners ();
-        const ExerciseStaking = await ethers.getContractFactory (
-          'Staking',
+        const ExerciseStaking = await ethers.getContractFactory ('Staking');
+        const exerciseStaking = new ethers.Contract (
+          contractAddr,
+          ExerciseStaking.interface,
           acc1
         );
-        const exerciseStaking = await ExerciseStaking.deploy ();
-        await exerciseStaking.deployed ();
 
         await exerciseStaking.connect (acc1).stakeMaticForExercise (1, 20, {
           value: ethers.utils.parseEther ('0.01'),
@@ -83,12 +93,19 @@ describe ('Exercise Staking', function () {
   });
 
   describe ('This should check the generation of NFT', function () {
-    it ('It will generate a new NFT from the contract', async function () {
+    it ('It will generate a new NFT from the contract', async function (
+      taskArgs
+    ) {
       try {
+        const contractAddr = taskArgs.contract;
+        const account = taskArgs.account;
         const [acc1, acc2] = await ethers.getSigners ();
-        const NFTContract = await ethers.getContractFactory ('MYNFT', acc1);
-        const nftContract = await NFTContract.deploy ();
-        await nftContract.deployed ();
+        const MYNFTStaking = await ethers.getContractFactory ('MYNFT');
+        const nftContract = new ethers.Contract (
+          contractAddr,
+          MYNFTStaking.interface,
+          acc1
+        );
 
         await nftContract.connect (acc1).safeMint (acc1.address);
         expect (await nftContract.connect (acc1).ownerOf (0)).to.equal (
@@ -111,19 +128,24 @@ describe ('Exercise Staking', function () {
     });
   });
   describe ('This will test our reward function', function () {
-    it ('This will test the reward function if the user wins', async () => {
+    it ('This will test the reward function if the user wins', async taskArgs => {
       try {
-        const [acc1, acc2, acc3] = await ethers.getSigners ();
-        const NFTContract = await ethers.getContractFactory ('MYNFT', acc1);
-        const nftContract = await NFTContract.deploy ();
-        await nftContract.deployed ();
-
-        const ExerciseStaking = await ethers.getContractFactory (
-          'Staking',
+        const contractAddr = taskArgs.contract;
+        const account = taskArgs.account;
+        const [acc1, acc2] = await ethers.getSigners ();
+        const MYNFTStaking = await ethers.getContractFactory ('MYNFT');
+        const nftContract = new ethers.Contract (
+          contractAddr,
+          MYNFTStaking.interface,
           acc1
         );
-        const exerciseStaking = await ExerciseStaking.deploy ();
-        await exerciseStaking.deployed ();
+
+        const ExerciseStaking = await ethers.getContractFactory ('Staking');
+        const exerciseStaking = new ethers.Contract (
+          contractAddr,
+          ExerciseStaking.interface,
+          acc1
+        );
 
         const amount = ethers.utils.parseEther ('0.01');
         await exerciseStaking.connect (acc2).stakeMaticForExercise (1, 20, {
@@ -135,7 +157,7 @@ describe ('Exercise Staking', function () {
 
         await exerciseStaking
           .connect (acc1)
-          .rewardMaticForExercise (1, acc2.address);
+          .rewardMaticForExercise (1, acc2.address, acc3.address);
         expect (await exerciseStaking.connect (acc1).ownerOf (0)).to.equal (
           acc2.address
         );
@@ -153,35 +175,40 @@ describe ('Exercise Staking', function () {
     });
     it ('This will test the loosing condition upon reward', async () => {
       try {
-        const [acc1, acc2] = await ethers.getSigners ();
-        const ExerciseStaking = await ethers.getContractFactory (
-          'Staking',
+        const [acc1, acc2, acc3] = await ethers.getSigners ();
+        const ExerciseStaking = await ethers.getContractFactory ('Staking');
+        const exerciseStaking = new ethers.Contract (
+          contractAddr,
+          ExerciseStaking.interface,
           acc1
         );
-        const exerciseStaking = await ExerciseStaking.deploy ();
-        await exerciseStaking.deployed ();
-        
+
         const oldBalance = await acc2.getBalance ();
-        
+
         const amount = ethers.utils.parseEther ('0.01');
-        const transaction = await exerciseStaking.connect (acc2).stakeMaticForExercise (1, 20, {
-          value: amount,
-        });
-        
+        const transaction = await exerciseStaking
+          .connect (acc2)
+          .stakeMaticForExercise (1, 20, {
+            value: amount,
+          });
+
         const recepient = await transaction.wait ();
         const gasUsed = recepient.gasUsed.mul (recepient.effectiveGasPrice);
-        
+
         await exerciseStaking
           .connect (acc1)
           .updateTargetDone (1, 15, acc2.address);
 
+        const oldCharityBalance = await acc3.getBalance ();
         await exerciseStaking
           .connect (acc1)
-          .rewardMaticForExercise (1, acc2.address);
-        
-        const amountt = await acc2.getBalance();
+          .rewardMaticForExercise (1, acc2.address, acc3.address);
+
+        const newCharityBalance = await acc3.getBalance ();
+        expect (newCharityBalance).to.equal (0.9 * 0.25 * oldCharityBalance);
+        const amountt = await acc2.getBalance ();
         expect (amountt).to.equal (
-            oldBalance.sub(0.25*amount).sub(gasUsed)
+          oldBalance.sub (0.25 * amount).sub (gasUsed)
         );
       } catch (err) {
         console.log (err);
